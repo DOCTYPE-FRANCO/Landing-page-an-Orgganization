dd946c318dbbd4537c93ca8718e41fbb3a1ee308
Franco <ufrancis186@gmail.com>
2025-09-04 14:59:34 +0100

    more pics added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import Treasurer from "../assets/treasurer.jpg";
 import Lawyer from "../assets/lawyer.jpg";
 import Provost from "../assets/provost.jpg";
 import PublicRel from "../assets/publicrel.jpg";
-import Orjison from "../assets/OrjisonChukwuka.jpg";
+import Orjison from "../assets/Orjisonedit.jpeg";
 import EmekaAnthony from "../assets/EnemchukwuAnthony.jpg";
 import VictorArase from "../assets/VictorArase.jpg";
 import NonsoFelix from "../assets/NonsoFelix.jpg";
@@ -15,28 +15,31 @@ import Joeigwe from "../assets/Joeigwe.jpg";
 import ChukwujekwuMadubuko from "../assets/ChukwujekwuMadubuko.jpg";
 import clementemeh from "../assets/clementemeh.jpg";
 import ChrisBuchi from "../assets/ChrisBuchi.jpg";
+import Okenwa from "../assets/Okenwaedit.jpeg";
+import EmekaUdeo from "../assets/Emekaedit.jpeg";
 
 function About() {
     const executives = [
-        
-        { name: 'President', image: President, role: 'President' },
-        { name: 'Vice President', image: VP, role: 'Vice President' },
-        { name: 'Treasurer', image: Treasurer, role: 'Treasurer' },
-        { name: 'Lawyer', image: Lawyer, role: 'Legal Counsel' },
-        { name: 'Provost', image: Provost, role: 'Provost' },
-        { name: 'Public Relations Officer', image: PublicRel, role: 'PR Officer' },
-        { name: 'Sir Chigozie Ijeomah ', image: Chairman, role: 'Chairman Board of Trustees' },
-        { name: 'Joe NWANNEBIKE Igwe ', image: Joeigwe, role: 'Disciplinary committee Chairman ' },
-        { name: 'Orjison chukwuka Charles ', image: Orjison, role: 'Member' },
-        { name: 'Enemchukwu Emeka Anthony ', image: EmekaAnthony, role: 'Member' },
-        { name: 'Mr. Victor Arase ', image: VictorArase, role: 'Member' },
-        { name: 'ORANUGO FELIX NONSO ', image: NonsoFelix, role: 'Member' },
-        { name: 'ChukwuEmeka Emmanuel Anosike', image: ChukwuemekaAnosike, role: 'Member' },
-        { name: 'Madubuko chukwujekwu Michael ', image: ChukwujekwuMadubuko, role: 'Member' },
-        { name: 'Emenike Clement Emeh ', image: clementemeh, role: 'Welfare - Member' },
-        { name: 'Nwosu Mmaduabuchi Christian ', image: ChrisBuchi, role: 'Member' },
-        
+        { name: 'President', image: President, role: 'President', business: 'Management' },
+        { name: 'Vice President', image: VP, role: 'Vice President', business: 'Management' },
+        { name: 'Treasurer', image: Treasurer, role: 'Treasurer', business: 'Finance' },
+        { name: 'Lawyer', image: Lawyer, role: 'Legal Counsel', business: 'Legal' },
+        { name: 'Provost', image: Provost, role: 'Provost', business: 'Academics' },
+        { name: 'Public Relations Officer', image: PublicRel, role: 'PR Officer', business: 'Communications' },
+        { name: 'Sir Chigozie Ijeomah', image: Chairman, role: 'Chairman Board of Trustees', business: 'Board' },
+        { name: 'Joe NWANNEBIKE Igwe', image: Joeigwe, role: 'Disciplinary Committee Chairman', business: 'Disciplinary' },
+        { name: 'Orjison chukwuka Charles', image: Orjison, role: 'Member', business: 'General' },
+        { name: 'Enemchukwu Emeka Anthony', image: EmekaAnthony, role: 'Member', business: 'General' },
+        { name: 'Mr. Victor Arase', image: VictorArase, role: 'Member', business: 'General' },
+        { name: 'ORANUGO FELIX NONSO', image: NonsoFelix, role: 'Member', business: 'General' },
+        { name: 'ChukwuEmeka Emmanuel Anosike', image: ChukwuemekaAnosike, role: 'Member', business: 'General' },
+        { name: 'Madubuko chukwujekwu ', image: ChukwujekwuMadubuko, role: 'Member', business: 'General' },
+        { name: 'Emenike Clement Emeh', image: clementemeh, role: 'Welfare - Member', business: 'Welfare' },
+        { name: 'Nwosu Mmaduabuchi Christian', image: ChrisBuchi, role: 'Member', business: 'General' },
+        { name: 'Chigozie Ijeaoma', image: Okenwa, role: 'Member', business: 'General' },
+        { name: 'Emeka Udeogaranya', image: EmekaUdeo, role: 'Member', business: 'General' },
     ];
+
     return (
         <div className="flex flex-col justify-center items-center overflow-hidden mb-[100px] ">
             <div>
@@ -63,7 +66,8 @@ function About() {
                             </div>
                             
                             <div className="flex flex-col gap-8 justify-center items-center">
-                                <h3 className="text-lg font-bold">{executive.name}</h3>
+                                <h3 className="text-lg font-bold">{executive.name.toLocaleUpperCase()}</h3>
+                                <p>{}</p>
                                 <p className="text-sm">{executive.role}</p>
                             </div>
                         </div>
