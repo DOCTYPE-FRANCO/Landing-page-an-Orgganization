b49e63534b54af9e45d252cf78b737e363ceb460
Franco <ufrancis186@gmail.com>
2025-09-18 15:42:53 +0100

    mobile nav fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,15 +24,15 @@ function Navbar(){
                 
                 { /*  Mobile View */}
 
-                <div className={`${mobile? "bg-white fixed w-full h-full bottom-0 right-0 left-0 mt-[400px] z-50  items-center flex flex-col" : "h-0 w-0"}  gap-20`}>
-                    <div  className="flex flex-col justify-end items-end w-full p-4">
+                <div className={`${mobile? "bg-white fixed w-full h-[250px] top-0 z-50 items-center flex flex-col" : "h-0 w-0"}  gap-20`}>
+                    <div  className="flex flex-col justify-end items-end w-full">
                         <img onClick={()=> setMobile(false)} src={X} className="w-[40px] h-[40px] justify-end "/>
                     </div>
-                    <ul className="text-3xl font-bold flex flex-col gap-20">
-                        <li onClick={()=> setMobile(false)} className="cursor-pointer hover:text-gray-600 font-bold"><Link to="/">Home</Link></li>
-                        <li onClick={()=> setMobile(false)} className="cursor-pointer hover:text-gray-600 font-bold"><Link to="/events">Events</Link></li> 
-                        <li onClick={()=> setMobile(false)} className="cursor-pointer hover:text-gray-600 font-bold"><Link to="/about">About</Link></li>
-                        <li onClick={()=> setMobile(false)} className="cursor-pointer hover:text-gray-600 font-bold"><Link to="/contact">Contact</Link></li>
+                    <ul className="text-2xl justify-center items-center font-bold flex flex-col gap-3  fixed top-20">
+                        <li onClick={()=> setMobile(false)} className="border border-black text-center w-[200px] items-center cursor-pointer hover:text-gray-600 font-bold"><Link to="/">HOME</Link></li>
+                        <li onClick={()=> setMobile(false)} className="border border-black text-center w-[200px] items-center cursor-pointer hover:text-gray-600 font-bold"><Link to="/events">EVENTS</Link></li> 
+                        <li onClick={()=> setMobile(false)} className="border border-black text-center w-[200px] items-center cursor-pointer hover:text-gray-600 font-bold"><Link to="/about">ABOUT</Link></li>
+                        <li onClick={()=> setMobile(false)} className="border border-black text-center w-[200px] items-center cursor-pointer hover:text-gray-600 font-bold"><Link to="/contact">CONTACT</Link></li>
                                                
                     </ul>
                 </div>
