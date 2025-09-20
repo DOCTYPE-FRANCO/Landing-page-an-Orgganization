d54c723b7712680eed6eee451a03b9219ac8b496
Franco <ufrancis186@gmail.com>
2025-09-20 18:04:56 +0100

    fix/ additions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,33 +13,35 @@ import VictorArase from "../assets/VictorArase.jpg";
 import NonsoFelix from "../assets/NonsoFelix.jpg";
 import ChukwuemekaAnosike from "../assets/ChukwuemekaAnosike.jpeg";
 import Joeigwe from "../assets/Joeigwe.jpg";
-import ChukwujekwuMadubuko from "../assets/ChukwujekwuMadubuko.jpg";
 import clementemeh from "../assets/clementemeh.jpg";
 import ChrisBuchi from "../assets/ChrisBuchi.jpg";
 import Okenwa from "../assets/Okenwaedit.jpeg";
 import EmekaUdeo from "../assets/Emekaedit.jpeg";
 import JudeDuru from "../assets/judeduru.jpg"
+import Chika from "../assets/ChikaEkwunife.jpg"
+import Callistus from "../assets/Callistus.jpg"
 
 function About() {
     const executives = [
         { name: 'Prince Anthony Okpala', image: President, role: 'President', business: 'Management' },
-        { name: 'Vice President', image: VP, role: 'Vice President', business: 'Management' },
-        { name: 'Treasurer', image: Treasurer, role: 'Treasurer', business: 'Finance' },
-        { name: 'Lawyer', image: Lawyer, role: 'Legal Counsel', business: 'Legal' },
+        { name: 'Callistus Chukwudubem', image: Callistus, role: 'Vice President', business: 'Management' },
+        { name: 'Madubuko chukwujekwu', image: Treasurer, role: 'Treasurer', business: 'Forex (Bureau de change)' },
+        { name: 'Tony Ezeani', image: Lawyer, role: 'Legal Counsel', business: 'Legal' },
         { name: 'Provost', image: Provost, role: 'Provost', business: 'Academics' },
-        { name: 'Public Relations Officer', image: PublicRel, role: 'PR Officer', business: 'Communications' },
+        { name: 'Godfrey Godfrey ', image: PublicRel, role: 'Public Relations Officer', business: 'Communications' },
         { name: 'Sir Chigozie Ijeomah', image: Chairman, role: 'Chairman Board of Trustees', business: 'CEO Lekki City Homes and Shelters LTD/Lekki Homemade restaurant and  bar. ' },
         { name: 'Joe NWANNEBIKE Igwe', image: Joeigwe, role: 'Disciplinary Committee Chairman', business: 'Real estate consultancy ' },
-        { name: 'Orjison chukwuka Charles', image: Orjison, role: 'Member', business: 'General' },
+        { name: 'Orjison chukwuka Charles', image: Orjison, role: 'Member', business: 'Business' },
         { name: 'Enemchukwu Emeka ', image: EmekaAnthony, role: 'Member', business: 'CEO Tonymeks Nigeria limited ' },
         { name: 'Mr. Victor Arase', image: VictorArase, role: 'Member', business: 'Businessman/CEO' },
         { name: 'ORANUGO FELIX NONSO', image: NonsoFelix, role: 'Member', business: 'CEO NONY OHAMS NIG LTD' },
         { name: 'ChukwuEmeka Emmanuel Anosike', image: ChukwuemekaAnosike, role: 'Member', business: 'Real Estate ' },
-        { name: 'Madubuko chukwujekwu ', image: ChukwujekwuMadubuko, role: 'Tresurer', business: 'Forex (Bureau de change)' },
         { name: 'Emenike Clement Emeh', image: clementemeh, role: 'Member', business: 'Real Estate' },
         { name: 'Nwosu Mmaduabuchi Christian', image: ChrisBuchi, role: 'Member', business: 'C.E.O De Xclusive Citadel Impex Ltd Dealers in Textile, Consultant, Contractor and Auctioneer with NPF.' },
-        { name: 'Emeka Udeogaranya', image: EmekaUdeo, role: 'Member', business: 'General' },
+        { name: 'Emeka Udeogaranya', image: EmekaUdeo, role: 'Member', business: 'Business' },
         { name: 'Mr jude Duru ', image: JudeDuru, role: 'Member', business: 'Lasduru global resources ' },
+        { name: 'Mr Chika Ekwunife ', image: Chika, role: 'Member', business: 'Business' },
+
     ];
 
     return (
@@ -74,7 +76,7 @@ function About() {
                             </div>
                             
                             <div className="flex flex-col gap-8 justify-center items-center">
-                                <h3 className="text-lg font-bold">{executive.name.toLocaleUpperCase()}</h3>
+                                <h3 className="text-lg font-bold text-center">{executive.name.toLocaleUpperCase()}</h3>
                                 <p className="text-gray-600 font-bold text-center">{executive.business}</p>
                                 <p className="text-sm font-semibold">{executive.role}</p>
                             </div>
